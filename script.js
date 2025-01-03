4d91182cf4b5a8373093c3b20c77d3be63407b45
Anvarbek <c1221sharp@gmail.com>
2025-01-03 17:35:05 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,94 @@
+// START
+
+var buttonColors = ["green", "red", "yellow", "blue"];
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+var level = 0;
+
+var started = false;
+
+// Star the game with keyboard
+document.addEventListener("keypress", function () {
+    if (!started) {
+        document.getElementById("level-title").textContent = "Level " + level;
+        nextSequence();
+        started = true;
+    }
+});
+
+// Click on the keyboard
+document.querySelectorAll(".btn").forEach(function (button) {
+    button.addEventListener("click", function () {
+        var userChosenColor = this.id;
+        userClickedPattern.push(userChosenColor);
+
+        playSound(userChosenColor);
+        animatePress(userChosenColor);
+
+        checkAnswer(userClickedPattern.length - 1);
+    });
+});
+
+// function for new Sequence
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    document.getElementById("level-title").textContent = "Level " + level;
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    setTimeout(function () {
+        var button = document.getElementById(randomChosenColor);
+        button.classList.add("pressed");
+        setTimeout(function () {
+            button.classList.remove("pressed");
+        }, 200);
+        playSound(randomChosenColor);
+    }, 500);
+}
+
+// Check Answer 
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(nextSequence, 1000);
+        }
+    } else {
+        playSound("wrong");
+        document.body.style.backgroundColor = "red";
+        setTimeout(function () {
+            document.body.style.backgroundColor = "#15477c";
+        }, 200);
+
+        document.getElementById("level-title").textContent = "Game Over, Press Any Key to Restart";
+        startOver();
+    }
+}
+
+// function for Sound
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+// Animation for click a elements
+function animatePress(currentColor) {
+    var activeButton = document.getElementById(currentColor);
+    activeButton.classList.add("pressed");
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+
+// Repeat game
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
